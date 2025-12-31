07e4ad174ea87b6bb8d6c4b980b8aa526369d5e2
Sahil Mane <148791198+sahilmane69@users.noreply.github.com>
2025-12-31 17:21:42 +0530

    Initial Commit

---
+++ DevTracker/DevTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevTrackerApp.swift
+//  DevTracker
+//
+//  Created by Sahil Mane on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DevTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
